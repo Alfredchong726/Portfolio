6e85205bdceff0d9a15b7a2bfe3fa8f21ad9f216
Alfredchong726 <alfredchong260@gmail.com>
2023-04-05 12:57:58 +0800

    remove the routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,7 @@ import ErrorPage from "@/scenes/ErrorPage";
 type Props = {};
 
 const App = (props: Props) => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
-  );
-  /* return <HomePage />; */
+  return <HomePage />;
 };
 
 export default App;
